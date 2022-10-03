d38ff53f4200b40c77d3f616dd0211a35dcfeb6f
karl dollmayer <karldollmayer@gmail.com>
2022-10-03 10:23:59 +0200

    update styles

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,87 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import MessageItem from '@my-fullstack-app/shared'
+
 import './App.css';
-import axios from 'axios'
 
-axios.defaults.baseURL = process.env.REACT_APP_API_KEY
+import { Route, Routes } from 'react-router-dom';
 
-const fetchMessages =async (): Promise<MessageItem[]> => {
-   const response = await axios.get<MessageItem[]>('/messages')
-   return response.data
-} 
+import MessagePage from './pages/MessagePage';
+
+
 
 
 
 function App() {
-  const [messages, setMessages] = useState<MessageItem[]>([])
-  const [error, setError] = useState<string | undefined>()
-  const [messageText, setMessageText] = useState<string>('')
-  const [userText, setUserText] = useState<string>('')
-
-  const waitingMessage: string = 'Waiting for messages...' 
-
-  const createMessage = (messageText: string, userText: string): void => {
-    localStorage.setItem('username', JSON.stringify(userText));
-    const messageItem: MessageItem = {
-      userName: userText,
-      messageText: messageText,
-      timeStamp: new Date()
   
-    }
-   axios.post<MessageItem[]>('/messages', messageItem)
-   .then((res) => {
-      setMessages(res.data)
-      setMessageText('')
-   })
-    
-  }
-  
-  useEffect(() => {
-    fetchMessages().then(setMessages).catch((error) => {
-      setMessages([])
-      setError('Something went wrong with fetching messages...')
-
-    })
-  }, [])
-
-  const output = () => {
-      if (error) {
-        return( <div > <h2>{error}</h2></div> )
-      } else if (messages) {
-         return( <> {
-            messages.map((item) => {
-              return (
-                <div key={item._id}>
-                <h3>{item.userName}</h3>
-                <p>{item.messageText}</p>
-                </div>
-              )
-            })
-          }
-          </>
-           )
-      } else {
-        return ( <div>
-          <h2>{waitingMessage}</h2>
-        </div> )
-      }
-  }
   return (
     <div className="App">
-      <header className="App-header">
-        
-       Chat-App
-       
-      </header>
-      <div className='App-hero'>
-       {output()}
-       
-      </div>
-      <div className=''>
-        <input type="text" placeholder='Message' value={messageText} onChange={(e) => setMessageText(e.target.value)} />
-        <input type="text" placeholder='Username' required value={userText} onChange={(e) => setUserText(e.target.value)} />
-        <button onClick={(e) => createMessage(messageText, userText)} >Send</button>
-      </div>
+      <Routes>
+      <Route path='/' element={<MessagePage/>}/>
+      <Route path='/messages' element={<MessagePage/>}/>
+    
+      
+      </Routes>
     </div>
   );
 }
---
+++ packages/frontend/src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.styled.css'
+
+
+export default function Header() {
+  return (
+    <div className='header'> 
+    <h1> Karls Messenger 
+
+    </h1>
+    </div>
+  )
+}
